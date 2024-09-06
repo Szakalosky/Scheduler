9bd22c595a71dd38f0d671c97a6d38ebcf9d73f6
Szakalosky <adixpc014@wp.pl>
2024-09-06 11:51:52 +0200

    small changes for appointment date

--- src/App.jsx
+++ src/App.jsx
@@ -452,21 +452,11 @@ const App = (props) => {
         const parsedStartDate = appointmentsData.startDate.toDate();
         const parsedEndDate = appointmentsData.endDate.toDate();
 
-        const formatDate = (date) => {
-          const year = date.getFullYear();
-          const month = String(date.getMonth() + 1).padStart(2, "0");
-          const day = String(date.getDate()).padStart(2, "0");
-          const hours = String(date.getHours()).padStart(2, "0");
-          const minutes = String(date.getMinutes()).padStart(2, "0");
-
-          return `${year}-${month}-${day} ${hours}:${minutes}`;
-        };
-
         return {
           ...appointmentsData,
           id: doc.id,
-          startDate: formatDate(parsedStartDate),
-          endDate: formatDate(parsedEndDate),
+          startDate: parsedStartDate,
+          endDate: parsedEndDate,
         };
       });
 
